0f6132e36c262bc21eb5779c7ac8ea1efe504275
cjerzak <connor.jerzak@gmail.com>
2024-01-30 14:14:48 -0500

    initial changes

---
+++ latenterror/R/LatentError_MainFxn.R
@@ -0,0 +1,3 @@
+#!/usr/bin/env Rscript#' MainFunction#'#' Implements fancy analysis#'#' @param x The input#' @return `x` Returns this. #' @export#'#' @details Details. #'#' @examples#'#' # Comment here#' #x <- MainFunction(x)#' @export#' @import Rfast#' @mdMainFunction <- function(x){
+  return(x) 
+}
