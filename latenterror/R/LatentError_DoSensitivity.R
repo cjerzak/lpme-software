ece161293882183f470e907f7be7165bb64c04ad
Connor Jerzak <cjerzak@Connors-MacBook-Pro.local>
2024-04-25 11:20:36 -0400

    up

---
+++ latenterror/R/LatentError_DoSensitivity.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+#' MainFunction
+#'
+#' Implements fancy analysis
+#'
+#' @param x The input
+#' @return `x` Returns this. 
+#' @export
+#'
+#' @details Details. 
+#'
+#' @examples
+#'
+#' # Comment here
+#' #x <- MainFunction(x)
+#' @export
+
+#' @import Rfast
+#' @md
+
+LatentErrorSensitivity <- function(x){
+  return(x) 
+}
+
+
+# core estimation function base model
+# estimation function
+# -- how many approaches to fix 
+# -- estimate degree of measurement error 
+# -- how much does split half affect us? 
+# -- results about how things differ in latent case 
+# correction functions 
+# -- 
+# diagnostic function
+
