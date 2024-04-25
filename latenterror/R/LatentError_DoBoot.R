d5c6ff7d9730d66a6ad4f065533261e44bb6a307
Connor Jerzak <cjerzak@Connors-MacBook-Pro.local>
2024-04-25 11:20:26 -0400

    up

--- latenterror/R/LatentError_DoBoot.R
+++ latenterror/R/LatentError_DoBoot.R
@@ -1,4 +1,24 @@
-#!/usr/bin/env Rscript#' MainFunction#'#' Implements fancy analysis#'#' @param x The input#' @return `x` Returns this. #' @export#'#' @details Details. #'#' @examples#'#' # Comment here#' #x <- MainFunction(x)#' @export#' @import Rfast#' @mdMainFunction <- function(x){
+#!/usr/bin/env Rscript
+#' LatentErrorBoot
+#'
+#' Implements fancy analysis
+#'
+#' @param x The input
+#' @return `x` Returns this. 
+#' @export
+#'
+#' @details Details. 
+#'
+#' @examples
+#'
+#' # Comment here
+#' #x <- LatentErrorBoot(x)
+#' @export
+
+#' @import Rfast
+#' @md
+
+LatentErrorBoot <- function(x){
   return(x) 
 }
 
