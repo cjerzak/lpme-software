676cb33702bafea175ee7b57c024b6b7028d783f
Connor Jerzak <cjerzak@Connors-MBP.cable.rcn.com>
2024-07-08 15:56:50 -0400

    up

--- latenterror/R/LatentError_DoOneRun.R
+++ latenterror/R/LatentError_DoOneRun.R
@@ -154,15 +154,3 @@ LatentOneRun <- function(Yobs,
        "VarEst_split" = var(x.est1 - x.est2) / 2 )
   )
 }
-
-
-# core estimation function base model
-# estimation function
-# -- how many approaches to fix 
-# -- estimate degree of measurement error 
-# -- how much does split half affect us? 
-# -- results about how things differ in latent case 
-# correction functions 
-# -- 
-# diagnostic function
-
